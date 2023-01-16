2236c4247f12888d6eccc91cb3a93fb0df4b2c36
cdrani <charlesdrani@gmail.com>
2023-01-16 01:24:01 -0700

    Replace netlify edge-functions for functions

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import svelte from "@astrojs/svelte";
 import vue from "@astrojs/vue";
 import image from "@astrojs/image";
 import tailwind from "@astrojs/tailwind";
-import netlify from "@astrojs/netlify/edge-functions";
+import netlify from "@astrojs/netlify/functions";
 
 // https://astro.build/config
 export default defineConfig({
